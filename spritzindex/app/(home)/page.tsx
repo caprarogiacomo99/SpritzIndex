2a3da826926dc552b799dcdd3fc0a16817660b11
caprarogiacomo99 <caprarogiacomo99@gmail.com>
2026-07-15 22:43:15 +0200

    Fix new

--- spritzindex/app/(home)/page.tsx
+++ spritzindex/app/(home)/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 import { supabase } from "@/utils/supabase/client";
 import { useState, useEffect } from "react";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/app/components/ui/select";
